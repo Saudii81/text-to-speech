100dcf9fb6ecf21b0dd413e42817aa433c783408
Saudii81 <isaudatu81@gmail.com>
2022-10-24 06:25:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+class TextToSpeech:
+    def __init__(self):
+        self.printText()
+
+    def printText(self):
+        print("Welcome, this is my awesome Text To Speech App")
+
+txt_to_speech = TextToSpeech()
