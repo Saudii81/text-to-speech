cf14b9ae00d4d89f96434c7bcd96bf7fa3f90db0
Saudii81 <isaudatu81@gmail.com>
2022-11-05 09:46:41 +0100

    build spectogram

--- main.py
+++ main.py
@@ -99,3 +99,66 @@ for i in range(n):
   ax.set_ylim([-1.1,1.1])
 
 plt.show()
+
+def get_spectrogram(waveform):
+  # Convert the waveform to a spectrogram via a STFT.
+  spectrogram = tf.signal.stft(
+      waveform, frame_length=255, frame_step=128)
+  # Obtain the magnitude of the STFT.
+  spectrogram = tf.abs(spectrogram)
+  # Add a `channels` dimension, so that the spectrogram can be used
+  # as image-like input data with convolution layers (which expect
+  # shape (`batch_size`, `height`, `width`, `channels`).
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+
+
+
+
+
+
+for i in range(3):
+  label = label_names[example_labels[i]]
+  waveform = example_audio[i]
+  spectrogram = get_spectrogram(waveform)
+
+  print('Label:', label)
+  print('Waveform shape:', waveform.shape)
+  print('Spectrogram shape:', spectrogram.shape)
+  print('Audio playback')
+  display.display(display.Audio(waveform, rate=16000))
+
+
+
+
+
+
+def plot_spectrogram(spectrogram, ax):
+  if len(spectrogram.shape) > 2:
+    assert len(spectrogram.shape) == 3
+    spectrogram = np.squeeze(spectrogram, axis=-1)
+  # Convert the frequencies to log scale and transpose, so that the time is
+  # represented on the x-axis (columns).
+  # Add an epsilon to avoid taking a log of zero.
+  log_spec = np.log(spectrogram.T + np.finfo(float).eps)
+  height = log_spec.shape[0]
+  width = log_spec.shape[1]
+  X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
+  Y = range(height)
+  ax.pcolormesh(X, Y, log_spec)
+
+
+
+
+
+
+fig, axes = plt.subplots(2, figsize=(12, 8))
+timescale = np.arange(waveform.shape[0])
+axes[0].plot(timescale, waveform.numpy())
+axes[0].set_title('Waveform')
+axes[0].set_xlim([0, 16000])
+
+plot_spectrogram(spectrogram.numpy(), axes[1])
+axes[1].set_title('Spectrogram')
+plt.suptitle(label.title())
+plt.show()
