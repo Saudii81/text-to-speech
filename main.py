72225bd568731041dcd8819ef238e6f906da0ee9
Saudii81 <isaudatu81@gmail.com>
2022-11-21 12:51:25 +0100

    making the specs

--- main.py
+++ main.py
@@ -355,3 +355,17 @@ for i in range(3):
 
 
 
+for i in range(3):
+  label = label_names[example_labels[i]]
+  waveform = example_audio[i]
+  spectrogram = get_spectrogram(waveform)
+
+  print('Label:', label)
+  print('Waveform shape:', waveform.shape)
+  print('Spectrogram shape:', spectrogram.shape)
+  print('Audio playback')
+  display.display(display.Audio(waveform, rate=16000))
+
+
+
+
